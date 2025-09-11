e41721ec174b04755cf81dd014608026719229d3
Victoria Tambasco <victoriaa.tambasco@gmail.com>
2025-09-11 20:39:11 -0300

    Update fila circular.h

--- fila circular/fila circular.h
+++ fila circular/fila circular.h
@@ -1,11 +1,19 @@
 #ifndef _H_FILA
 #define _H_FILA
 
-struct fila* criar_fila();
-bool ehCheia(struct fila* f);
-bool ehVazia(struct fila* f);
-void push(struct fila* f, int item);
-int pop(struct fila* f);
-int obtem_elemento(struct fila* f);
+#include <stdbool.h>
+
+#define TAM 5
+
+typedef struct fila Fila;
+
+Fila* criar_fila();
+bool ehCheia(Fila* f);
+bool ehVazia(Fila* f);
+void push(Fila* f, int valor);
+int pop(Fila* f);
+int obtem_elemento(Fila* f);
+void mostrar_fila(Fila* f);
+void liberar_fila(Fila* f);
 
 #endif // _H_FILA
